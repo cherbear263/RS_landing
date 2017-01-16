c235f27c1e953e50a044f08ffdbcb0a470bf7d48
Cheryl Jones <cheryl@real.social>
2017-01-16 11:01:11 +1000

    created files, used start bootstrap theme freelancer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
 
 // Set the banner content
 var banner = ['/*!\n',
@@ -15,28 +21,46 @@ var banner = ['/*!\n',
     ' */\n',
     ''
 ].join('');
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+var sassdir = './sass/**/*.scss';
+var cssdir = './css';
 
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/freelancer.less')
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
+
+gulp.task('sass', function() {
+    return gulp.src([sassdir])
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+    });
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    });
 });
+
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/freelancer.css')
+gulp.task('minify-css', function() {
+    return gulp.src('css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
-});
+        }));
+    });
+
+
 
 // Minify JS
 gulp.task('minify-js', function() {
@@ -47,16 +71,16 @@ gulp.task('minify-js', function() {
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'));
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('vendor/jquery'));
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -66,27 +90,25 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
+        .pipe(gulp.dest('vendor/font-awesome'));
+});
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', [ 'sass', 'minify-css', 'minify-js', 'copy']);
 
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
-})
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
+//tasks that run when you type gulp watch
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch(sassdir, ['sass']);
+    gulp.watch('/css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload)
+
+    //when there is a change, log a message in the console
+    .on('change', function(event) {
+        console.log('File' + event.path + ' was ' + event.type + ' , running tasks...');
+    });
 });
+
+
